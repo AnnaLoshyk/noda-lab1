66e993b5a15cb339d6624c8b2dab39475c6ba26f
AnnaLoshyk <loshykanna@gmail.com>
2024-02-16 14:16:41 +0200

    dopka

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
-const i = 0 + 3;
-const a = "String";
-console.log(a);
-console.log(i);
+import {createForwardProxy} from './proxy.js';
+
+const HOST = '0.0.0.0';
+const PORT = 8080;
+
+
+const server = createForwardProxy({
+    debug: true
+});
+
+server.listen(PORT, HOST, () => {
+    console.log(`Connection established on ${HOST}:${PORT}`);
+});
--- proxy.js
+++ proxy.js
@@ -1,37 +1,98 @@
-import http from "http";
+import {createConnection, createServer} from 'node:net';
 
-const Server = http.createServer((incomingRequest, outgoingResponse) => {
-    const destinationUrl = `http://www.testingmcafeesites.com/${incomingRequest.url}`;
-    const options = new URL(destinationUrl);
-    options.method = incomingRequest.method;
-    options.headers = incomingRequest.headers;
-    options.headers.host = "testingmcafeesites.com"; // Set the Host header
+const HTTP_SPLITTER = '\r\n';
+const HTTP_HEAD_SPLITTER = '\r\n\r\n';
 
-    const forwardedRequest = http.request(options, (remoteResponse) => {
-        outgoingResponse.writeHead(
-            remoteResponse.statusCode,
-            remoteResponse.headers,
-        );
-        remoteResponse.pipe(outgoingResponse, {
-            end: true,
+const HTTP_CONNECT = 'CONNECT';
+const HTTP_PROTOCOL = 'HTTP/1.1';
+const HTTP_STATUS_OK = '200 Ok';
+const HTTP_STATUS_INTERNAL = '500 Internal Server Error';
+const HTTP_STATUS_BAD_REQUEST = '400 Bad Request';
+
+/**
+ * Create forward proxy server
+ * @param options.debug {boolean} Enable debugging
+ * */
+export function createForwardProxy(options = {}) {
+    const server = createServer();
+
+    server.on('connection', socket => {
+        socket.once('data', async buffer => {
+            try {
+                const head = parseHead(buffer);
+
+                if (!head.headers.host) {
+                    return socket.end(`${HTTP_PROTOCOL} ${HTTP_STATUS_BAD_REQUEST} ${HTTP_HEAD_SPLITTER}`);
+                }
+
+                const [hostname, port] = head.headers.host.split(':');
+
+                if (!hostname) {
+                    return socket.end(`${HTTP_PROTOCOL} ${HTTP_STATUS_BAD_REQUEST} ${HTTP_HEAD_SPLITTER}`);
+                }
+
+                if (options.debug) {
+                    console.log(head);
+                }
+
+                const proxySocket = createConnection({host: hostname, port: port || 80});
+
+
+
+                if (head.method === HTTP_CONNECT) {
+                    socket.write(`${HTTP_PROTOCOL} ${HTTP_STATUS_OK} ${HTTP_HEAD_SPLITTER}`);
+                } else {
+                    proxySocket.write(buffer);
+                }
+
+                socket.pipe(proxySocket);
+                proxySocket.pipe(socket);
+
+                proxySocket.on('error', () => {
+                    socket.end(`${HTTP_PROTOCOL} ${HTTP_STATUS_INTERNAL} ${HTTP_HEAD_SPLITTER}`);
+                });
+
+                socket.on('error', () => {
+                    socket.end(`${HTTP_PROTOCOL} ${HTTP_STATUS_INTERNAL} ${HTTP_HEAD_SPLITTER}`);
+                });
+            } catch (e) {
+                if (options.debug) {
+                    console.error(e.stack);
+                }
+            }
         });
     });
 
-    incomingRequest.pipe(forwardedRequest, {
-        end: true,
-    });
+    return server;
+}
 
-    forwardedRequest.on("error", (error) => {
-        console.error("Request error:", error.message);
-        outgoingResponse.writeHead(500, {
-            "Content-Type": "text/plain",
-        });
-        outgoingResponse.end("Server Error");
-    });
-});
+/*
+* Parse a first socket message
+* */
+function parseHead(buffer) {
+    const headers = {};
 
-const SERVER_PORT = 8080;
+    const [head, ...rawHeaders] = buffer.toString().split(HTTP_HEAD_SPLITTER)[0].split(HTTP_SPLITTER);
+    const [method, url, protocol] = head.split(' ');
 
-Server.listen(SERVER_PORT, () => {
-    console.log(`server on port: ${SERVER_PORT}`);
-});
+    for (const rawHeader of rawHeaders) {
+        if (!rawHeader) {
+            continue;
+        }
+
+        const [key, ...values] = rawHeader.split(':');
+
+        if (!key) {
+            continue;
+        }
+
+        headers[key.toLowerCase().trim()] = values.join(':').trim();
+    }
+
+    return {
+        method,
+        protocol,
+        headers,
+        url,
+    };
+}
